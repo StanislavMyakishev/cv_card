1f2e8cf155589691967977ba002ae3ea64d12716
StanislavMyakishev <stanislaw.myakishev@yandex.ru>
2024-02-18 20:53:41 +0300

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+npm run build
+cd out
+git init
+git remote add origin https://github.com/StanislavMyakishev/stanislavmyakishev.github.io.git
+git add .
+git commit -m 'repo update'
+git push origin master --force
