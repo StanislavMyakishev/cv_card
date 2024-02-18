719737b6a1fa3a71be9723c6e0069f6a75991179
StanislavMyakishev <stanislaw.myakishev@yandex.ru>
2024-02-18 20:55:17 +0300

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ git init
 git remote add origin https://github.com/StanislavMyakishev/stanislavmyakishev.github.io.git
 git add .
 git commit -m 'repo update'
+git branch -M master
 git push origin master --force
